8600d6a98ef182561f4f2728306398c876fdc76e
Émerson Yuri <45644801+FlossyYuri@users.noreply.github.com>
2020-07-13 13:08:55 +0200

    Backend

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ const http = require('http');
 
 const routes = require('./routes');
 const { setupWebsocket } = require('./websocket');
-const { MONGO_URL } = require('../.env');
+const url = process.env.MONGO_URL;
+if (!url) const { MONGO_URL } = require('../.env');
 const app = express();
 const server = http.Server(app);
-const url = MONGO_URL;
 const dev = app.get('env') !== 'production';
-if (!dev) {
-  url = process.env.MONGO_URL;
-}
 
 setupWebsocket(server);
 mongoose.connect(url || MONGO_URL, {
